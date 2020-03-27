68d12e6ec2ca8e4accc6db2cb993bfa27fbd3c6d
Gustavo <gustavopch@gmail.com>
2020-03-27 11:45:13 -0300

    refactor: use `ObjectId` type from "bson" instead of "mongodb"
    They mean the same thing but are actually different classes that break
    our tests when compared by Jest's `toEqual`.

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
-import { ObjectId } from 'mongodb'
+import { ObjectId } from 'bson'
 
 export type BaseDocument = {
   _id: ObjectId
--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,4 +1,4 @@
-import { ObjectId } from 'mongodb'
+import { ObjectId } from 'bson'
 
 import { buildCursor, sanitizeLimit } from './utils'
 
